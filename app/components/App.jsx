fbb2f828134d1f99f6561da02c7e5e0b56482bf5
comdotlinux <guru@linux.com>
2016-03-06 01:24:14 +0530

    Further updates

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -19,8 +19,8 @@ export default class App extends React.Component {
     validationState() {
         let length = this.state.value.length;
         console.log("input length " + length);
-        if (length > 5) return 'success';
-        else if (length > 2) return 'warning';
+        if (length > 20) return 'success';
+        else if (length > 10) return 'warning';
         else if (length > 0) return 'error';
     }
     
@@ -33,19 +33,20 @@ export default class App extends React.Component {
   render () {
     return (<div>
                 <p>TODO APPLICATION</p>
-                    <form onSubmit="{this.handleSubmit}">
-                        <div>
-                           <Input type="text"  
-                                    value={this.state.value} 
-                                    placeholder="todo goes here"
-                                    label="Enter A TODO"
-                                    bsStyle={this.validationState()}
-                                    hasFeedback
-                                    ref="input"
-                                    groupClassName="input-group"
-                                    labelClassName="input-group-addon"
-                                    onChange={this.handleChange.bind(this)} />
-                            <Button bsStyle="primary">Add TODO</Button>
+                    <form onSubmit={this.handleSubmit}>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="input-group">
+                                   <Input type="text"  
+                                            value={this.state.value} 
+                                            placeholder="Please enter a good descriptive todo...."
+                                            bsStyle={this.validationState()}
+                                            hasFeedback
+                                            ref="input"
+                                            onChange={this.handleChange.bind(this)} />
+                                    <Button bsStyle="primary">Add TODO</Button>
+                                </div>
+                            </div>
                         </div>
                     </form>
             </div>);
