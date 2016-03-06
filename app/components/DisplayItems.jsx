419384ddc8e4a85d5ca74bdca9412d829a3cad1d
comdotlinux <guru@linux.com>
2016-03-07 01:44:33 +0530

    Added nice table not working

--- app/components/DisplayItems.jsx
+++ app/components/DisplayItems.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Input, ButtonInput } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
 export default class DisplayItems extends React.Component {
     
@@ -8,17 +8,26 @@ export default class DisplayItems extends React.Component {
     }
 
     render(){
-    return <div>
-        <ul>
-        {this.props.todoItems.map((item, i) => {
-                return <li 
-                            key={item}> {item} 
-                                    <a href='#' onClick={this.props.handleDelete.bind(this,item)}>[X]</a>
-                        </li>;
-            })
-        }
-        </ul>
-    </div>;
-    
+    return <Table striped bordered condensed hover>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Done</th>
+                    <th>Todo Description</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                        {this.props.todoItems.map((item, i) => {
+                            return <tr key={item + '-' + i}>
+                                        <td key={item + '-' + i + 'index'}>{i}</td>
+                                        <td key={item + '-' + i + 'done'}>done</td>
+                                        <td key={item + '-' + i + 'description'}>{item}</td>
+                                        <td key={item + '-' + i + 'delete'}><a href='#' onClick={this.props.handleDelete.bind(null,item)}>[X]</a></td>
+                                    </tr>;
+                                })
+                        }
+                </tbody>
+           </Table>;
     }
 }
